64ea173d6487447aa19dd2b3d7a58a1c494b79ff
Rafaela Bustamante <rafaelabdm@gmail.com>
2025-03-29 09:45:55 -0300

    refactor: turn solution into separate functions

--- biggest_island/bigest_island.py
+++ biggest_island/bigest_island.py
@@ -4,30 +4,8 @@
 # Each cell is considered connected to other cells horizontally or vertically (not diagonally).
 
 
-# There are more Example Matrices in the file examples.py, test it out!
-matrix = [
-    [1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1],
-    [1, 1, 0, 1, 1, 0],
-    [0, 1, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0, 1],
-]
-
-
 # SOLUTION:
-
-islands = list()
-temp_island = set()
-
-row = 0
-slot = 0
-end_search = False
-
-rows_count = len(matrix)
-slots_per_row = len(matrix[0])
-
-
-def check_islands(position: tuple) -> bool:
+def check_discovered_islands(position: tuple, islands: list[list]) -> bool:
     """
         Checks for slot in already discovered islands.
     """
@@ -37,7 +15,7 @@ def check_islands(position: tuple) -> bool:
     return False
 
 
-def check_next_slots(current_row: int, current_slot: int) -> None:
+def check_next_slots(matrix: list[list], temp_island: set[tuple], current_row: int, current_slot: int, slots_per_row: int) -> None:
     """
         Checks if slots on the right of a land spot belongs to the current island,
         therefore linking the current slot to the island as well.
@@ -58,46 +36,67 @@ def check_next_slots(current_row: int, current_slot: int) -> None:
             break
 
 
-def find_biggest_island(islands: list[list]) -> int:
+def get_biggest_island(islands: list[list]) -> int:
     bigest_island = 0
     for island in islands:
         if len(island) > bigest_island:
             bigest_island = len(island)
+    return bigest_island
 
 
-while row < rows_count and not end_search:
-    while slot < slots_per_row:
-        if not temp_island and matrix[row][slot] == 1 and (row, slot) and not check_islands((row, slot)):
-            temp_island.add((row, slot))
-            slot += 1
-            continue
-        if matrix[row][slot] == 1:
-            if check_islands((row, slot)) or (row, slot) in temp_island:
+def search_islands(matrix: list[list]) -> list[list]:
+    islands = list()
+    temp_island = set()
+
+    row = 0
+    slot = 0
+    end_search = False
+
+    rows_count = len(matrix)
+    slots_per_row = len(matrix[0])
+
+    while row < rows_count and not end_search:
+        while slot < slots_per_row:
+            if not temp_island and matrix[row][slot] == 1 and (row, slot) and not check_discovered_islands((row, slot), islands):
+                temp_island.add((row, slot))
                 slot += 1
                 continue
-            slot_up = None if row - 1 < 0 else row - 1
-            slot_left =  None if slot - 1 < 0 else slot - 1
-            if set([(row, slot_left), (slot_up, slot)]).intersection(temp_island):
-                temp_island.add((row, slot))
+            if matrix[row][slot] == 1:
+                if check_discovered_islands((row, slot), islands) or (row, slot) in temp_island:
+                    slot += 1
+                    continue
+                slot_up = None if row - 1 < 0 else row - 1
+                slot_left =  None if slot - 1 < 0 else slot - 1
+                if set([(row, slot_left), (slot_up, slot)]).intersection(temp_island):
+                    temp_island.add((row, slot))
+                else:
+                    check_next_slots(matrix, temp_island, row, slot, slots_per_row)
+                    slot += 1
             else:
-                check_next_slots(row, slot)
                 slot += 1
+                continue
+
+        if temp_island and row + 1 == rows_count:
+            islands.append(temp_island.copy())
+            temp_island.clear()
+            row=0
+            slot=0
+        elif not temp_island and row == rows_count and slot == slots_per_row:
+            end_search = True
         else:
-            slot += 1
-            continue
-
-    if temp_island and row + 1 == rows_count:
-        islands.append(temp_island.copy())
-        temp_island.clear()
-        row=0
-        slot=0
-    elif not temp_island and row == rows_count and slot == slots_per_row:
-        end_search = True
-    else:
-        row += 1
-        slot = 0 
+            row += 1
+            slot = 0 
+    
+    return islands
 
 
-print(f"The biggest island has {find_biggest_island(islands)} slots of land!")
+# There are more Example Matrices in the file examples.py, test it out!
+from examples import *
+
+def main():
+    islands = search_islands(matrix_1)
+    print(f"This matrix have {len(islands)} islands.")
+    print(f"The biggest one has {get_biggest_island(islands)} slots of land!")
 
 
+main()
